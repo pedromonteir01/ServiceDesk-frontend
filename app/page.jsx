8d381e86c4b7f3d011fd5f3988e6e1a6a18cdbea
ArthBG <arthurborges.senai564@gmail.com>
2024-09-23 14:57:55 -0300

    feat: update home page content
    Update the content of the home page to provide a better description of the Service Desk functionality. This includes adding a title and a description with information about the purpose and benefits of the Service Desk. Also, commented out some unused code for future reference.
    
    Fixes #123

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+// |
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <div className={styles.container}>
+      {/* <HeaderSecundary /> */}
+      {/* <div className={styles.imagebanner}>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={72}
+        height={16}
+      />
+      </div> */}
+      <h1 className={styles.title}>
+        Conheça a nossa funcionalidade 
+      </h1>
+      <p className={styles.description}>
+       O Service Desk funciona como um "balcão de atendimento virtual", responsável por acolher e endereçar solicitações de suporte. Sua função é garantir a satisfação do cliente. Portanto, demanda agilidade, bom senso de priorização e capacidade de direcionamento e resolução efetiva. O que procuramos é a agilidade para processos de manutenção na infraestrutura do SENAI-Valinhos. 
+      </p>
+      </div>
   );
 }
