6908a8072174601cdb15448d1374ffb55182c89b
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-09-23 09:47:14 -0300

    feat: implement with css

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -1,8 +1,24 @@
 import { useState, useContext } from "react";
 import { UserContext } from "../contexts/userContext";
+import style from './login.module.css'
 
 export default function Login() {
+
+    const { user, setUser } = useContext(UserContext);
+
     return (
-        <main></main>
+        <main className={style.container}>
+            {
+                user ? (
+                    <article>
+                        <p>logged</p>
+                    </article>
+                ) : (
+                    <article>
+                        { /* login */ }
+                    </article>
+                )   
+            }
+        </main>
     );
 }
