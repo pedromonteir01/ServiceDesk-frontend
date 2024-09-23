f051e7082707047a3f30d3eae43336a7c24cc034
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-09-23 09:38:17 -0300

    feat: initial structure Login

---
+++ app/Login/page.jsx
@@ -0,0 +1,8 @@
+import { useState, useContext } from "react";
+import { UserContext } from "../contexts/userContext";
+
+export default function Login() {
+    return (
+        <main></main>
+    );
+}
