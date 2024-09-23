211191c925155d6b81c5248afee6acf18504ffd3
Gabriel Caurin <gabrielcaurin@gmail.com>
2024-09-23 11:25:04 -0300

    feat: add logo but no show

--- app/Request/page.jsx
+++ app/Request/page.jsx
@@ -1,11 +1,18 @@
 import Header from "../Components/Header/Header";
 import styles from "./request.module.css";
+import Image from "next/image";
 export default function Request() {
   return (
     <div className={styles.generalDiv}>
       <Header />
       <div>
-        <h1>Request</h1>
+        <Image
+          src="/senaicerto.png"
+          alt="Logo do Senai"
+          width={300}
+          height={300}
+          className={styles.logo}
+        />
       </div>
     </div>
   );
