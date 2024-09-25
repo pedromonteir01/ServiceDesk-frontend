0b9ca0c899e40da5dbf5af0bacc0685ee92d3727
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-09-25 07:56:53 -0300

    fix: package.json

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -1,20 +1,28 @@
-import Image from "next/image";
-import Link from "next/link";
-
-
+import styles from "./header.module.css";
 export default function Header() {
-    return (
-        <header className="header">
-        <Link href="/">
-            <a>
-            <Image
-                src="/logo.svg"
-                alt="logo"
-                width={100}
-                height={100}
-            />
-            </a>
-        </Link>
-        </header>
-    );
-    }
+  return (
+    <div className={styles.generalDiv}>
+      <div className={styles.senai}></div>
+
+      <div className={styles.menu}>
+        <h3 className={styles.links}>
+          <a href="https://www.fiesp.com.br/">FIESP</a>
+        </h3>
+        <h3 className={styles.links}>
+          <a href="https://www.ciesp.com.br/">CIESP</a>
+        </h3>
+        <h3 className={styles.links}>
+          <a href="https://www.sesisp.org.br/">SESI</a>
+        </h3>
+        <h3 className={styles.links}>
+          <a href="https://www.sp.senai.br/">SENAI</a>
+        </h3>
+        <h3 className={styles.links}>
+          <a href="https://www.fiesp.com.br/instituto-roberto-simonsen-irs/">
+            IRS
+          </a>
+        </h3>
+      </div>
+    </div>
+  );
+}
