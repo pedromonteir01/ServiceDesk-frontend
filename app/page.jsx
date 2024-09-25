c36ded15b96b635ca599f4cb2a0dd2434aa5399b
Pedro Isac <pedro.isac@aluno.senai.br>
2024-09-25 08:30:15 -0300

    Remove unnecessary footer component import

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Footer from "./Components/Footer/Footer";
 
 export default function Home() {
   return (
     <main>
-      <Footer />
     </main>
   );
 }
