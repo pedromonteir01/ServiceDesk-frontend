19b64325857f7af7d14421852279837c67a05429
Pedro Isac <pedro.isac@aluno.senai.br>
2024-09-25 08:29:33 -0300

    feat: Adicionar ícones de redes sociais no rodapé
    Adiciona os ícones das redes sociais Facebook, Youtube e Instagram no rodapé da página. Também configura a animação de fade-up para os ícones e adiciona estilos de cores e tamanhos aos ícones.
    
    Refatora o CSS do rodapé para adicionar uma seção de ícones sociais e uma seção inferior com informações de direitos autorais.
    
    Atualiza as dependências do projeto para incluir a biblioteca Aos na versão 2.3.4.
    
    [ISSUE-123]

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Footer from "./Components/Footer/Footer";
 
 export default function Home() {
   return (
     <main>
-      
+      <Footer />
     </main>
   );
 }
