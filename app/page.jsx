677978457a54a34fd960808d4ac2fa55a827c36f
ArthBG <arthurborges.senai564@gmail.com>
2024-09-25 08:24:36 -0300

    Refactor page component to include header and footer components

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-// |
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      {/* <HeaderSecundary /> */}
+      <Header />
       {/* <div className={styles.videobanner}>
       <Image
         src="/vercel.svg"
@@ -33,6 +34,7 @@ export default function Home() {
       <div className={styles.buttonFunction}>
         <button className={styles.button}>Reportar Problema</button>
         </div>
+      <Footer />
       </div>
   );
 }
