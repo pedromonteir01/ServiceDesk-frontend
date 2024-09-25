0687955784c0b8b293a6ebc4901bba831dbc3c3e
ArthBG <arthurborges.senai564@gmail.com>
2024-09-25 08:06:54 -0300

    feat: update home page content
    - Updated the home page content to include a video banner and additional text sections.
    - Added a new section to report problems and a button to report issues.

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       {/* <HeaderSecundary /> */}
-      {/* <div className={styles.imagebanner}>
+      {/* <div className={styles.videobanner}>
       <Image
         src="/vercel.svg"
         alt="Vercel Logo"
@@ -14,12 +14,25 @@ export default function Home() {
         height={16}
       />
       </div> */}
+      <div className={styles.falsedivbase}>
+        <h1 className={styles.falsetitle}>
+          Video
+        </h1>
+      </div>
+      <div className={styles.containerText}>
       <h1 className={styles.title}>
         Conheça a nossa funcionalidade 
       </h1>
       <p className={styles.description}>
        O Service Desk funciona como um "balcão de atendimento virtual", responsável por acolher e endereçar solicitações de suporte. Sua função é garantir a satisfação do cliente. Portanto, demanda agilidade, bom senso de priorização e capacidade de direcionamento e resolução efetiva. O que procuramos é a agilidade para processos de manutenção na infraestrutura do SENAI-Valinhos. 
       </p>
+      </div>
+        <h1 className={styles.titleFunction}>
+          Identificou alguma falha?
+        </h1>
+      <div className={styles.buttonFunction}>
+        <button className={styles.button}>Reportar Problema</button>
+        </div>
       </div>
   );
 }
