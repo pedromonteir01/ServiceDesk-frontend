505cc77e474029f7de4cc80dbb441e08f1e2c9f3
caiquenaimi <caique.naimi@aluno.senai.br>
2024-09-25 11:43:53 -0300

    Refactorizar o componente Footer para adicionar endereço completo e aumentar o z-index do texto de direitos autorais

--- app/Components/Footer/Footer.jsx
+++ app/Components/Footer/Footer.jsx
@@ -32,7 +32,7 @@ export default function Footer() {
         </div>
       </div>
       <div className={styles.footerBottom}>
-        <h1 /* data-aos="fade-up" */>Local do SENAI Valinhos</h1>
+        <h2 /* data-aos="fade-up" */>R. Artur Fernandes Querido, 55 - Vila Santo Antonio, Valinhos - SP, 13270-530</h2>
         <p /* data-aos="fade-up" */ className={styles.copyrightText}>
           &copy; 2024 BFlow. Todos os direitos reservados.
         </p>
