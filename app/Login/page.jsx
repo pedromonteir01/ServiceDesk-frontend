aaede579a61f419ac62ef759f8cda148cbe4c54d
elPedron <pedrohenriquesilva@aluno.senai.br>
2024-09-29 17:30:49 -0300

    fix: header imports

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -1,6 +1,6 @@
 import style from './login.module.css'
 import UserComponent from "../components/UserComponent/UserComponent";
-import Header from '../Components/Header/header';
+import Header from '../Components/Header/Header';
 import Footer from '../Components/Footer/Footer';
 
 export default function Login() {
