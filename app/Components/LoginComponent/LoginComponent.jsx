a3f8f467b4be52fab52e5589b9976ebb28123f85
Caique <caiquenaimi.564@gmail.com>
2024-09-29 16:51:40 -0300

    Refatorar estilos do componente LoginComponent e melhorar a aparência e responsividade

--- app/Components/LoginComponent/LoginComponent.jsx
+++ app/Components/LoginComponent/LoginComponent.jsx
@@ -2,61 +2,60 @@
 import { useState } from "react";
 import { getUserByEmail } from "@/app/actions/users";
 import { useRouter } from "next/navigation";
-import style from '@/app/Login/login.module.css'
+import style from '@/app/Login/login.module.css';
 
 const LoginComponent = ({ setUser }) => {
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const login = async(email, password) => {
-        const user = await getUserByEmail(email);
-        console.log(user);
-        
-        if(user) {
-           if(user.password == password) {
-            setUser(user)
-            router.replace('/');
-           }
-        } 
+  const login = async (email, password) => {
+    const user = await getUserByEmail(email);
+    console.log(user);
+
+    if (user) {
+      if (user.password === password) {
+        setUser(user);
+        router.replace('/');
+      }
     }
+  };
 
-    return (
-        <article>
-            { /* login */}
+  return (
+    <div className={style.loginContainer}>
+      <article className={style.loginBox}>
+        <h2 className={style.loginTitle}>Faça o Login</h2>
 
-            <h2>Faça o Login</h2>
+        <section className={style.inputField}>
+          <label htmlFor="email" className={style.label}>Email:</label>
+          <input
+            type="text"
+            name="email"
+            value={email}
+            className={style.input}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </section>
 
-            { /* input field */}
-            <section className={style.inputField}>
-                <label htmlFor="email">Email: </label>
-                <input
-                    type="text"
-                    name="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </section>
+        <section className={style.inputField}>
+          <label htmlFor="password" className={style.label}>Senha:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            className={style.input}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </section>
 
-            { /* input field */}
-            <section className={style.inputField}>
-                <label htmlFor="password">Senha: </label>
-                <input
-                    type="text"
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </section>
-
-            <section className={style.btnLogin}>
-                <button className={style.btn} onClick={() => login(email, password)}>ENTRAR</button>
-            </section>
-
-        </article>
-    )
-}
+        <section className={style.btnLogin}>
+          <button className={style.btn} onClick={() => login(email, password)}>ENTRAR</button>
+        </section>
+      </article>
+    </div>
+  );
+};
 
 export default LoginComponent;
--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -1,10 +1,14 @@
 import style from './login.module.css'
 import UserComponent from "../components/UserComponent/UserComponent";
+import Header from '../Components/Header/header';
+import Footer from '../Components/Footer/Footer';
 
 export default function Login() {
     return (
         <main className={style.container}>
+            <Header/>
             <UserComponent/>
+            <Footer/>
         </main>
     );
 }
