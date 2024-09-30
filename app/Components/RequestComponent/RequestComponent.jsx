e6a9422ccd4e5bfb632c537b5a4190f384365a8e
caiquenaimi <caique.naimi@aluno.senai.br>
2024-09-30 10:28:12 -0300

    Refactorizar estilos de CSS en el módulo de inicio de sesión y pie de página

---
+++ app/Components/RequestComponent/RequestComponent.jsx
@@ -0,0 +1,64 @@
+"use client";
+import styles from "./requestComponent.module.css";
+import Image from "next/image";
+
+import { RiMenuSearchLine } from "react-icons/ri";
+import { CiSearch } from "react-icons/ci";
+import RenderTest from "../RenderTest/renderTest";
+
+export default function RequestComponent() {
+  const teste = () => {
+    console.log("chamando requisição");
+  };
+
+  return (
+    <>
+      <div className={styles.init}>
+        <div>
+          <Image
+            src="/senaicerto.png"
+            alt="Logo do Senai"
+            width={200}
+            height={200}
+            className={styles.logo}
+          />
+        </div>
+        <div>
+          <CiSearch color="#000" size={30} />
+        </div>
+      </div>
+
+      <div className={styles.requestButton}>
+        <div className={styles.filter}>
+          <RiMenuSearchLine color="#ff0000" size={30} />
+        </div>
+        <div>
+          <button className={styles.buttonRequest} onClick={teste}>
+            Adicionar Requisição
+          </button>
+        </div>
+      </div>
+
+      <RenderTest
+        local="SALA 1"
+        desc="Lâmpada queimada"
+        autor="Pedro Moneteiro"
+        status="PENDENTE"
+      />
+
+      <RenderTest
+        local="SALA 2"
+        desc="Ar condicionado quebrado"
+        autor="Caique Naimi"
+        status="CONCLUIDO"
+      />
+
+      <RenderTest
+        local="BANHEIRO"
+        desc="Privada quebrada"
+        autor="Arthur Borges"
+        status="PENDENTE"
+      />
+    </>
+  );
+}
--- app/Request/page.jsx
+++ app/Request/page.jsx
@@ -1,12 +1,7 @@
-"use client";
 import Header from "../Components/Header/Header";
 import styles from "./request.module.css";
-import Image from "next/image";
-
-import { RiMenuSearchLine } from "react-icons/ri";
-import { CiSearch } from "react-icons/ci";
 import Footer from "../Components/Footer/Footer";
-import RenderTest from "../Components/RenderTest/renderTest";
+import RequestComponent from "../Components/RequestComponent/RequestComponent";
 
 export default function Request() {
   const teste = () => {
@@ -16,55 +11,8 @@ export default function Request() {
   return (
     <div className={styles.generalDiv}>
       <Header />
-      <div className={styles.init}>
-        <div>
-          <Image
-            src="/senaicerto.png"
-            alt="Logo do Senai"
-            width={200}
-            height={200}
-            className={styles.logo}
-          />
-        </div>
-        <div>
-          <CiSearch color="#000" size={30} />
-        </div>
-      </div>
-
-      <div className={styles.requestButton}>
-        <div className={styles.filter}>
-          <RiMenuSearchLine color="#ff0000" size={30} />
-        </div>
-        <div>
-          <button className={styles.buttonRequest} onClick={teste}>
-            Adicionar Requisição
-          </button>
-        </div>
-      </div>
-
-      <RenderTest
-        local="SALA 1"
-        desc="Lâmpada queimada"
-        autor="Pedro Moneteiro"
-        status="PENDENTE"
-      />
-
-      <RenderTest
-        local="SALA 2"
-        desc="Ar condicionado quebrado"
-        autor="Caique Naimi"
-        status="CONCLUIDO"
-      />
-
-      <RenderTest
-        local="BANHEIRO"
-        desc="Privada quebrada"
-        autor="Arthur Borges"
-        status="PENDENTE"
-      />
-      <div>
+      <RequestComponent />
       <Footer />
-      </div>
     </div>
   );
 }
