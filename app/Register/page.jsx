c3364639da8b80edb2b006957aa0b39b1ab5d4ea
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-09-30 09:07:14 -0300

    feat: components in register

--- app/Register/page.jsx
+++ app/Register/page.jsx
@@ -1,7 +1,13 @@
+import Footer from '../Components/Footer/Footer';
+import Header from '../Components/Header/Header';
+import styles from './register.module.css'
+
 export default function Register() {
     return (
-        <main>
-
+        <main className={styles.container}>
+            <Header/>
+            
+            <Footer/>
         </main>
     );
 }
