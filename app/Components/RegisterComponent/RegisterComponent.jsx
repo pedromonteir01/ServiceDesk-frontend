77e178abc6942baade496e1b60fec9a0b5a67a82
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-09-30 10:30:00 -0300

    feat: structure register

--- app/Components/RegisterComponent/RegisterComponent.jsx
+++ app/Components/RegisterComponent/RegisterComponent.jsx
@@ -1,8 +1,62 @@
-import styles from './registerComponent.module.css'
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import styles from '../../Register/register.module.css'
 
 const RegisterComponent = () => {
+
+    const router = useRouter();
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <article>
+        <article className={styles.loginBox}>
+
+            <h2 className={styles.loginTitle}>Faça o Login</h2>
+
+            <form onSubmit={(e) => {
+                e.preventDefault();
+            }}>
+
+                <section className={styles.inputField}>
+                    <label htmlFor="name" className={styles.label}>Nome:</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={name}
+                        className={styles.input}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </section>
+
+                <section className={styles.inputField}>
+                    <label htmlFor="email" className={styles.label}>Email:</label>
+                    <input
+                        type="text"
+                        name="email"
+                        value={email}
+                        className={styles.input}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </section>
+
+                <section className={styles.inputField}>
+                    <label htmlFor="password" className={styles.label}>Senha:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        className={styles.input}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </section>
+
+                <section className={styles.btnLogin}>
+                    <button className={styles.btn}>CADASTRAR</button>
+                </section>
+            </form>
 
         </article>
     );
--- app/Register/page.jsx
+++ app/Register/page.jsx
@@ -1,12 +1,13 @@
 import Footer from '../Components/Footer/Footer';
 import Header from '../Components/Header/Header';
+import RegisterComponent from '../Components/RegisterComponent/RegisterComponent';
 import styles from './register.module.css'
 
 export default function Register() {
     return (
         <main className={styles.container}>
             <Header/>
-            
+            <RegisterComponent/>
             <Footer/>
         </main>
     );
