33575cd5a4d245c2580d3ed9ed38d95cf66ab1d8
ArthBG <arthurborges.senai564@gmail.com>
2024-09-30 15:33:14 -0300

    Refactor SideHeader component to fix positioning issue and add full height

--- app/Components/SideHeader/sideHeader.jsx
+++ app/Components/SideHeader/sideHeader.jsx
@@ -1,7 +1,5 @@
-
-"use client";
-import { useState, useRef } from 'react';
-import Hamburger from 'hamburger-react';
+'use client';
+import React, { useState, useRef } from 'react';
 import { Sidebar } from 'primereact/sidebar';
 import { Button } from 'primereact/button';
 import { Avatar } from 'primereact/avatar';
@@ -9,99 +7,71 @@ import { Ripple } from 'primereact/ripple';
 import { StyleClass } from 'primereact/styleclass';
 import style from './sideHeader.module.css';
 
-const SideHeader = () => {
-    const [visible, setVisible] = useState(false);
-    const [isOpen, setOpen] = useState(false);
-    const btnRef1 = useRef(null);
-    const btnRef2 = useRef(null);
-    const btnRef3 = useRef(null);
-    const btnRef4 = useRef(null);
-
-    const toggle = () => {
-        setVisible(!visible);
-        console.log(visible);
-        
-    }
+const SideHeader = ({ isOpen }) => {
+    const [visible, setVisible] = useState(isOpen);
+    const btnRefs = useRef({});
     return (
-        <div className="card flex justify-content-center h-100vh">
-            <div className={style.sideHeader}>
-            <Button type="button" style={style.button} onClick={toggle}>
-                <Hamburger />
-             </Button>
-             </div>
-             <div className={style.side}>
-            <Sidebar
-                visible={visible}
-                onHide={() => setVisible(false)}
-                content={({ closeIconRef, hide }
-
-
-
-                ) => (
-                    <div>
-                        <div className="border-right-1" style={{ width: '280px', height: '100vh', position: 'absolute', top: 50, left:0 }}>
-                            <div className="flex flex-column h-full">
-                                <div className="flex align-items-center justify-content-between px-4 pt-3">
-                                    <span className="inline-flex align-items-center gap-2">
-                                      
-                                        <span className="font-semibold text-2xl text-primary">
-                                            SENAI
-                                        </span>
-                                    </span>
-                                    
-                                </div>
-                                <div className="overflow-y-auto">
-                                    <ul className="list-none p-3 m-0">
-                                        <li>
-                                            <StyleClass nodeRef={btnRef1} selector="@next" enterActiveClassName="slidedown" leaveToClassName="hidden" leaveActiveClassName="slideup">
-                                                <div ref={btnRef1} className="p-ripple p-3 flex align-items-center justify-content-between text-600 cursor-pointer">
-                                                    <span className="font-medium">FAVORITES</span>
-                                                    <i className="pi pi-chevron-down"></i>
-                                                    <Ripple />
-                                                </div>
-                                            </StyleClass>
-                                            <ul className="list-none p-0 m-0 overflow-hidden">
-                                                <li>
-                                                    <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                                                        <i className="pi pi-home mr-2"></i>
-                                                        <span className="font-medium">Dashboard</span>
-                                                        <Ripple />
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                                                        <i className="pi pi-bookmark mr-2"></i>
-                                                        <span className="font-medium">Bookmarks</span>
-                                                        <Ripple />
-                                                    </a>
-                                                </li>
-                                                <li>
-                                                    <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                                                        <i className="pi pi-cog mr-2"></i>
-                                                        <span className="font-medium">Settings</span>
-                                                        <Ripple />
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div className="mt-auto">
-                                    <hr className="mb-3 mx-3 border-top-1 border-none surface-border" />
-                                    <a className="m-3 flex align-items-center cursor-pointer p-3 gap-2 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
-                                        <Avatar image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png" shape="circle" />
-                                        <span className="font-bold">Amy Elsner</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
-            ></Sidebar>
+      <Sidebar
+        visible={visible}
+        onHide={() => setVisible(true)}
+        content={({ closeIconRef, hide }) => (
+          <div className="border-right-1" style={{ width: '280px', height: '100vh', position: 'absolute', top: 50, left: 0 }}>
+            <div className="flex flex-column h-full">
+              <div className="flex align-items-center justify-content-between px-4 pt-3">
+                <span className="inline-flex align-items-center gap-2">
+                  <span className="font-semibold text-2xl text-primary">
+                    SENAI
+                  </span>
+                </span>
+              </div>
+              <div className="overflow-y-auto">
+                <ul className="list-none p-3 m-0">
+                  <li>
+                    <StyleClass nodeRef={btnRefs.favorites} selector="@next" enterActiveClassName="slidedown" leaveToClassName="hidden" leaveActiveClassName="slideup">
+                      <div ref={(el) => { btnRefs.favorites = el; }} className="p-ripple p-3 flex align-items-center justify-content-between text-600 cursor-pointer">
+                        <span className="font-medium">FAVORITES</span>
+                        <i className="pi pi-chevron-down"></i>
+                        <Ripple />
+                      </div>
+                    </StyleClass>
+                    <ul className="list-none p-0 m-0 overflow-hidden">
+                      <li>
+                        <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
+                          <i className="pi pi-home mr-2"></i>
+                          <span className="font-medium">Dashboard</span>
+                          <Ripple />
+                        </a>
+                      </li>
+                      <li>
+                        <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
+                          <i className="pi pi-bookmark mr-2"></i>
+                          <span className="font-medium">Bookmarks</span>
+                          <Ripple />
+                        </a>
+                      </li>
+                      <li>
+                        <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
+                          <i className="pi pi-cog mr-2"></i>
+                          <span className="font-medium">Settings</span>
+                          <Ripple />
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              <div className="mt-auto">
+                <hr className="mb-3 mx-3 border-top-1 border-none surface-border" />
+                <a className="m-3 flex align-items-center cursor-pointer p-3 gap-2 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
+                  <Avatar image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png" shape="circle" />
+                  <span className="font-bold">Amy Elsner</span>
+                </a>
+              </div>
             </div>
-        </div>
-    )
-}
+          </div>
+        )}
+      ></Sidebar>
+    );
+};
 
 export default SideHeader;
-        
