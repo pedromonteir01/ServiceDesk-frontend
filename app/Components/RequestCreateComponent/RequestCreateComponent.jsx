c974e863812f5571f7297332ddeb3d36f7ae19b8
Caique <caiquenaimi.564@gmail.com>
2024-10-01 18:41:28 -0300

    Refactorizar componente RenderTeste para adicionar botões de remoção e edição

--- app/Components/RequestCreateComponent/RequestCreateComponent.jsx
+++ app/Components/RequestCreateComponent/RequestCreateComponent.jsx
@@ -1,39 +1,100 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./requestCreateComponent.module.css";
 import { IoCloudDownloadOutline } from "react-icons/io5";
-
+import { createRequest } from "@/app/actions/request";
+import { useRouter } from "next/navigation";
 
 export default function RequestCreateComponent() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [local, setLocal] = useState("");
+  const [image, setImage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const router = useRouter();
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setImage(file);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = {
+      title,
+      description,
+      local,
+      image,
+      status_request: "inconclued",
+      date_request: new Date().toISOString(),
+    };
+
+    const result = await createRequest(formData);
+
+    if (result.error) {
+      setErrorMessage(result.message);
+      setSuccessMessage("");
+    } else {
+      setSuccessMessage(result.message);
+      setErrorMessage("");
+      setTitle("");
+      setDescription("");
+      setLocal("");
+      setImage(null);
+
+      router.push("/RequestComponent");
+    }
+  };
+
   return (
     <div className={styles.main}>
       <h1 className={styles.title}>
         Relate aqui seu <span className={styles.problemText}>problema</span>
       </h1>
+      {errorMessage && <div className={styles.error}>{errorMessage}</div>}
+      {successMessage && <div className={styles.success}>{successMessage}</div>}
 
-      <label className={styles.label}>O que aconteceu? Descreva</label>
-      <textarea
-        className={styles.descriptionInput}
-      />
+      <form onSubmit={handleSubmit}>
+        <label className={styles.label}>O que aconteceu? Descreva</label>
+        <textarea
+          className={styles.descriptionInput}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
 
-      <label className={styles.label}>Imagem</label>
-      <div className={styles.imageUpload}>
-        <input type="file" accept="image/*" className={styles.fileInput} />
-        <IoCloudDownloadOutline color="#000" fontSize={30} />
-        <span>Inserir imagem</span>
-      </div>
-      <p className={styles.imageHint}>
-        Tire uma foto para facilitar a localização
-      </p>
+        <label className={styles.label}>Imagem</label>
+        <div className={styles.imageUpload}>
+          <input
+            type="file"
+            accept="image/*"
+            className={styles.fileInput}
+            onChange={handleImageChange}
+          />
+          <IoCloudDownloadOutline color="#000" fontSize={30} />
+          <span>Inserir imagem</span>
+        </div>
+        <p className={styles.imageHint}>
+          Tire uma foto para facilitar a localização
+        </p>
 
-      <label className={styles.label}>Qual foi o local?</label>
-      <select className={styles.select}>
-        <option value="">Selecione o ambiente</option>
-        {/* Adicione mais opções aqui */}
-        <option value="sala1">Sala 1</option>
-        <option value="sala2">Sala 2</option>
-        <option value="laboratorio">Laboratório</option>
-      </select>
+        <label className={styles.label}>Qual foi o local?</label>
+        <select
+          className={styles.select}
+          value={local}
+          onChange={(e) => setLocal(e.target.value)}
+        >
+          <option value="">Selecione o ambiente</option>
+          <option value="sala1">Sala 1</option>
+          <option value="sala2">Sala 2</option>
+          <option value="laboratorio">Laboratório</option>
+        </select>
 
-      <button className={styles.submitButton}>Criar Requisição</button>
+        <button className={styles.submitButton}>Criar Requisição</button>
+      </form>
     </div>
   );
 }
--- app/actions/request.js
+++ app/actions/request.js
@@ -6,65 +6,82 @@ const api = process.env.URL;
 
 export const getAllRequests = async () => {
   try {
-    const response = await axios.get(`${api}/requests`, { cache: 'no-store' });
+    const response = await axios.get(`${api}/requests`, { cache: "no-store" });
     if (response.data.length === 0) {
       return { message: "none_requests" };
     }
     return { results: response.data.length, requests: response.data };
   } catch (e) {
-    console.log('Error fetching data:', e);
+    console.log("Error fetching data:", e);
     return { error: "Error fetching requests", message: e.message };
   }
 };
 
 export const getRequestById = async (id) => {
   try {
-    const response = await axios.get(`${api}/requests/${id}`, { cache: 'no-store' });
+    const response = await axios.get(`${api}/requests/${id}`, {
+      cache: "no-store",
+    });
     if (response.data) {
       return { request: response.data };
     }
     return { error: 404, message: `Request not found with this id: ${id}` };
   } catch (e) {
-    console.log('Error fetching data:', e);
+    console.log("Error fetching data:", e);
     return { error: "Error fetching request", message: e.message };
   }
 };
 
 export const getRequestByLocal = async (local) => {
   try {
-    const response = await axios.get(`${api}/requests/local/${local}`, { cache: 'no-store' });
+    const response = await axios.get(`${api}/requests/local/${local}`, {
+      cache: "no-store",
+    });
     if (response.data.length > 0) {
       return { requests: response.data };
     }
-    return { error: 404, message: `Requests not found with this local: ${local}` };
+    return {
+      error: 404,
+      message: `Requests not found with this local: ${local}`,
+    };
   } catch (e) {
-    console.log('Error fetching data:', e);
+    console.log("Error fetching data:", e);
     return { error: "Error fetching requests by local", message: e.message };
   }
 };
 
 export const getRequestByStatus = async (status) => {
   try {
-    const response = await axios.get(`${api}/requests/status/${status}`, { cache: 'no-store' });
+    const response = await axios.get(`${api}/requests/status/${status}`, {
+      cache: "no-store",
+    });
     if (response.data.length > 0) {
       return { results: response.data.length, requests: response.data };
     }
-    return { error: 404, message: `Requests not found with this status: ${status}` };
+    return {
+      error: 404,
+      message: `Requests not found with this status: ${status}`,
+    };
   } catch (e) {
-    console.log('Error fetching data:', e);
+    console.log("Error fetching data:", e);
     return { error: "Error fetching requests by status", message: e.message };
   }
 };
 
 export const getRequestByUser = async (user) => {
   try {
-    const response = await axios.get(`${api}/requests/user/${user}`, { cache: 'no-store' });
+    const response = await axios.get(`${api}/requests/user/${user}`, {
+      cache: "no-store",
+    });
     if (response.data.length > 0) {
       return { results: response.data.length, requests: response.data };
     }
-    return { error: 404, message: `Requests not found with this user: ${user}` };
+    return {
+      error: 404,
+      message: `Requests not found with this user: ${user}`,
+    };
   } catch (e) {
-    console.log('Error fetching data:', e);
+    console.log("Error fetching data:", e);
     return { error: "Error fetching requests by user", message: e.message };
   }
 };
@@ -72,11 +89,15 @@ export const getRequestByUser = async (user) => {
 export const createRequest = async (request) => {
   try {
     const response = await axios.post(`${api}/requests`, request, {
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
-    return { status: 'success', message: 'Request created', data: response.data };
+    return {
+      status: "success",
+      message: "Request created",
+      data: response.data,
+    };
   } catch (e) {
-    console.log('Error creating request:', e);
+    console.log("Error creating request:", e);
     return { error: "Error creating request", message: e.message };
   }
 };
@@ -84,11 +105,15 @@ export const createRequest = async (request) => {
 export const updateRequest = async (id, request) => {
   try {
     const response = await axios.put(`${api}/requests/${id}`, request, {
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
-    return { status: 'success', message: 'Request updated', data: response.data };
+    return {
+      status: "success",
+      message: "Request updated",
+      data: response.data,
+    };
   } catch (e) {
-    console.log('Error updating request:', e);
+    console.log("Error updating request:", e);
     return { error: "Error updating request", message: e.message };
   }
 };
@@ -96,21 +121,33 @@ export const updateRequest = async (id, request) => {
 export const deleteRequest = async (id) => {
   try {
     const response = await axios.delete(`${api}/requests/${id}`);
-    return { status: 'success', message: 'Request deleted', data: response.data };
+    return {
+      status: "success",
+      message: "Request deleted",
+      data: response.data,
+    };
   } catch (e) {
-    console.log('Error deleting request:', e);
+    console.log("Error deleting request:", e);
     return { error: "Error deleting request", message: e.message };
   }
 };
 
 export const concludeStatus = async (id, status) => {
   try {
-    const response = await axios.patch(`${api}/requests/${id}/conclude`, { status }, {
-      headers: { 'Content-Type': 'application/json' },
-    });
-    return { status: 'success', message: 'Request concluded', data: response.data };
+    const response = await axios.patch(
+      `${api}/requests/${id}/conclude`,
+      { status },
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    return {
+      status: "success",
+      message: "Request concluded",
+      data: response.data,
+    };
   } catch (e) {
-    console.log('Error concluding request:', e);
+    console.log("Error concluding request:", e);
     return { error: "Error concluding request", message: e.message };
   }
 };
