496e6193f278b5f54e00a43172510ddf464a2272
Caique <caiquenaimi.564@gmail.com>
2024-10-01 20:40:52 -0300

    Refactorizar componente RenderTeste para adicionar botões de remoção e edição

--- app/Components/RequestCreateComponent/RequestCreateComponent.jsx
+++ app/Components/RequestCreateComponent/RequestCreateComponent.jsx
@@ -1,9 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./requestCreateComponent.module.css";
 import { IoCloudDownloadOutline } from "react-icons/io5";
 import { createRequest } from "@/app/actions/request";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function RequestCreateComponent() {
   const [title, setTitle] = useState("");
@@ -51,50 +52,61 @@ export default function RequestCreateComponent() {
   };
 
   return (
-    <div className={styles.main}>
-      <h1 className={styles.title}>
-        Relate aqui seu <span className={styles.problemText}>problema</span>
-      </h1>
-      {errorMessage && <div className={styles.error}>{errorMessage}</div>}
-      {successMessage && <div className={styles.success}>{successMessage}</div>}
+    <>
+      <Image
+        src="/senaicerto.png"
+        alt="Logo do Senai"
+        width={200}
+        height={200}
+        className={styles.logo}
+      />
+      <div className={styles.main}>
+        <h1 className={styles.title}>
+          Relate aqui seu <span className={styles.problemText}>problema</span>
+        </h1>
+        {errorMessage && <div className={styles.error}>{errorMessage}</div>}
+        {successMessage && (
+          <div className={styles.success}>{successMessage}</div>
+        )}
 
-      <form onSubmit={handleSubmit}>
-        <label className={styles.label}>O que aconteceu? Descreva</label>
-        <textarea
-          className={styles.descriptionInput}
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-
-        <label className={styles.label}>Imagem</label>
-        <div className={styles.imageUpload}>
-          <input
-            type="file"
-            accept="image/*"
-            className={styles.fileInput}
-            onChange={handleImageChange}
+        <form onSubmit={handleSubmit}>
+          <label className={styles.label}>O que aconteceu? Descreva</label>
+          <textarea
+            className={styles.descriptionInput}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
-          <IoCloudDownloadOutline color="#000" fontSize={30} />
-          <span>Inserir imagem</span>
-        </div>
-        <p className={styles.imageHint}>
-          Tire uma foto para facilitar a localização
-        </p>
 
-        <label className={styles.label}>Qual foi o local?</label>
-        <select
-          className={styles.select}
-          value={local}
-          onChange={(e) => setLocal(e.target.value)}
-        >
-          <option value="">Selecione o ambiente</option>
-          <option value="sala1">Sala 1</option>
-          <option value="sala2">Sala 2</option>
-          <option value="laboratorio">Laboratório</option>
-        </select>
+          <label className={styles.label}>Imagem</label>
+          <div className={styles.imageUpload}>
+            <input
+              type="file"
+              accept="image/*"
+              className={styles.fileInput}
+              onChange={handleImageChange}
+            />
+            <IoCloudDownloadOutline color="#000" fontSize={30} />
+            <span>Inserir imagem</span>
+          </div>
+          <p className={styles.imageHint}>
+            Tire uma foto para facilitar a localização
+          </p>
 
-        <button className={styles.submitButton}>Criar Requisição</button>
-      </form>
-    </div>
+          <label className={styles.label}>Qual foi o local?</label>
+          <select
+            className={styles.select}
+            value={local}
+            onChange={(e) => setLocal(e.target.value)}
+          >
+            <option value="">Selecione o ambiente</option>
+            <option value="sala1">Sala 1</option>
+            <option value="sala2">Sala 2</option>
+            <option value="laboratorio">Laboratório</option>
+          </select>
+
+          <button className={styles.submitButton}>Criar Requisição</button>
+        </form>
+      </div>
+    </>
   );
 }
