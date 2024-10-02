d71e78e95204ced3d3bbc12cdd164eb21e0ece87
caiquenaimi <caique.naimi@aluno.senai.br>
2024-10-02 08:15:32 -0300

    feat: add RequestCreate page and styles
    Add a new page component for creating requests, along with its corresponding CSS styles.

---
+++ app/RequestCreate/page.jsx
@@ -0,0 +1,14 @@
+import Header from "../Components/Header/Header";
+import styles from "./page.module.css";
+import Footer from "../Components/Footer/Footer";
+import RequestCreateComponent from "../Components/RequestCreateComponent/RequestCreateComponent";
+
+export default function RequestCreate() {
+  return (
+    <div className={styles.generalDiv}>
+      <Header />
+      <RequestCreateComponent />
+      <Footer />
+    </div>
+  );
+}
