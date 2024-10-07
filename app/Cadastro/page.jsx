4ac838640ebb5eb4dd70bfc37d1ef846b02a4969
Pedro Isac <isacp8220@gmail.com>
2024-10-07 10:57:40 -0300

    Remove: Header

--- app/Cadastro/page.jsx
+++ app/Cadastro/page.jsx
@@ -3,6 +3,7 @@ import { useState, useContext } from "react";
 import style from "./cadastro.module.css";
 import axios from "axios";
 import Image from "next/image";
+import Footer from "../Components/Footer/Footer";
 
 export default function Cadastro() {
   const [nome, setNome] = useState("");
@@ -31,42 +32,58 @@ export default function Cadastro() {
     }
   };
   return (
-    <div className={style.loginContainer}>
-      {/* Cadastro */}
-      <article className={style.loginBox}>
-        <section>
+    <main>
+      <div className={style.loginContainer}>
+        {/* Cadastro */}
+        <article className={style.loginBox}>
+          <section>
             <Image src="/logoSesi.svg" alt="Logo" width={280} height={80} />
-          <h2 className={style.loginTitle}>Crie sua conta para acessar</h2>
-        </section>
+            <h2 className={style.loginTitle}>Crie sua conta para acessar</h2>
+          </section>
 
-        {/* input field */}
-        <section className={style.inputField}>
-          <label htmlFor="name" className={style.label}>Nome: </label>
-          <input type="text" name="Nome" className={style.input}/>
-        </section>
+          {/* input field */}
+          <section className={style.inputField}>
+            <label htmlFor="name" className={style.label}>
+              Nome:{" "}
+            </label>
+            <input type="text" name="Nome" className={style.input} />
+          </section>
 
-        {/* input field */}
-        <section className={style.inputField}>
-          <label htmlFor="email" className={style.label}>Email educacional: </label>
-          <input type="text" name="email" className={style.input}/>
-        </section>
+          {/* input field */}
+          <section className={style.inputField}>
+            <label htmlFor="email" className={style.label}>
+              Email educacional:{" "}
+            </label>
+            <input type="text" name="email" className={style.input} />
+          </section>
 
-        {/* input field */}
-        <section className={style.inputField}>
-          <label htmlFor="password" className={style.label}>Senha: </label>
-          <input type="text" name="password" className={style.input}/>
-        </section>
+          {/* input field */}
+          <section className={style.inputField}>
+            <label htmlFor="password" className={style.label}>
+              Senha:{" "}
+            </label>
+            <input type="text" name="password" className={style.input} />
+          </section>
 
-        {/* input field */}
-        <section className={style.inputField}>
-          <label htmlFor="password" className={style.label}>Confirmar senha: </label>
-          <input type="text" name="password" value={confirmarSenha} className={style.input}/>
-        </section>
+          {/* input field */}
+          <section className={style.inputField}>
+            <label htmlFor="password" className={style.label}>
+              Confirmar senha:{" "}
+            </label>
+            <input
+              type="text"
+              name="password"
+              value={confirmarSenha}
+              className={style.input}
+            />
+          </section>
 
-        <section className={style.btnLogin}>
-          <button className={style.btn}>ENTRAR</button>
-        </section>
-      </article>
-    </div>
+          <section className={style.btnLogin}>
+            <button className={style.btn}>ENTRAR</button>
+          </section>
+        </article>
+      </div>
+      <Footer />
+    </main>
   );
 }
