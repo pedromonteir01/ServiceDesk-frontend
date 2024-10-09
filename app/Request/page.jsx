6ef5b6d749ecac696be6c8a72f2c8d940812aff0
Gabriel Caurin <gabrielcaurin@gmail.com>
2024-10-09 07:43:05 -0300

    Refactor RequestComponent: Remove unused imports and context

--- app/Request/page.jsx
+++ app/Request/page.jsx
@@ -2,8 +2,6 @@ import Header from "../Components/Header/Header";
 import styles from "./page.module.css";
 import Footer from "../Components/Footer/Footer";
 import RequestComponent from "../Components/RequestComponent/RequestComponent";
-import { useContext } from "react";
-import { UserContext } from "../contexts/userContext";
 
 export default function Request() {
   return (
