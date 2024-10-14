13fc5c743ce5066f148c8bac07226ce25645c692
Caique <caiquenaimi.564@gmail.com>
2024-10-14 18:47:56 -0300

    Refactorizar estilos da página do usuário e adicionar funcionalidade de exibir imagem do Gravatar

--- app/Components/UserPage/UserPage.jsx
+++ app/Components/UserPage/UserPage.jsx
@@ -1,36 +1,39 @@
-'use client';
 import { useContext, useEffect, useState } from 'react';
-import styles from './userPage.module.css'
+import styles from './userPage.module.css';
 import { UserContext } from '@/app/contexts/userContext';
+import gravatarUrl from 'gravatar-url'; // Importando a função do Gravatar
 
 const UserPage = () => {
     const { user } = useContext(UserContext);
     console.log(user);
 
     const [email, setEmail] = useState(user.email);
+    const [avatarUrl, setAvatarUrl] = useState(''); // Estado para armazenar a URL da imagem
 
     const showEmail = () => {
         let [localPart, domain] = email.split('@');
-    
         let localChars = localPart.split('');
-    
         for (let i = 3; i < localChars.length; i++) {
             localChars[i] = '*';
         }
-    
-        let response = localChars.join('') + '@' + domain;    
+        let response = localChars.join('') + '@' + domain;
         setEmail(response);
     };
-    
+
+    const getGravatarImage = () => {
+        const url = gravatarUrl(user.email, { size: 200, default: 'retro' });
+        setAvatarUrl(url); 
+    };
 
     useEffect(() => {
         showEmail();
+        getGravatarImage();
     }, []);
 
     return (
         <article className={styles.container}>
-                {/* Imagem senai */}
             <section className={styles.info}>
+                <img src={avatarUrl} alt="Avatar do usuário" className={styles.avatar} />
                 <h2>SEJA BEM-VINDO, {user.name.split(' ')[0].toUpperCase()}!</h2>
                 <div className={styles.content}>
                     <p className={styles.txt}>Nome: {user.name}</p>
