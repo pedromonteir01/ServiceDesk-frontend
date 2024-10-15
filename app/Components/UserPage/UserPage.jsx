dc86dcda4ce7dcb7e820b3d7026918ed9313b794
elPedron <pedrohenriquesilva@aluno.senai.br>
2024-10-14 21:21:21 -0300

    feat: verify isadmin

--- app/Components/UserPage/UserPage.jsx
+++ app/Components/UserPage/UserPage.jsx
@@ -23,7 +23,7 @@ const UserPage = () => {
 
     const getGravatarImage = () => {
         const url = gravatarUrl(user.email, { size: 200, default: 'retro' });
-        setAvatarUrl(url); 
+        setAvatarUrl(url);
     };
 
     useEffect(() => {
@@ -33,21 +33,31 @@ const UserPage = () => {
 
     return (
         <article className={styles.container}>
-            <section className={styles.info}>
-                <img src={avatarUrl} alt="Avatar do usuário" className={styles.avatar} />
-                <h2>SEJA BEM-VINDO, {user.name.split(' ')[0].toUpperCase()}!</h2>
-                <div className={styles.content}>
-                    <label className={styles.labelfor}>Nome:</label>
-                    <p className={styles.txt}>{user.name.toUpperCase()}</p>
-                    <label className={styles.labelfor}>Email:</label>
-                    <span className={styles.txt}>{email}</span>
-                    <label className={styles.labelfor}>Função:</label>
-                    <span className={styles.txt}>{user.isstudent ? 'Estudante' : 'Funcionário'}</span>
-                    <button className={styles.button} onClick={
-                        edit ? () => setEdit(false) : () => setEdit(true)
-                    }>{edit ? 'CANCELAR' : 'EDITAR'}</button>
-                </div>
-            </section>
+            {
+                user.isAdmin ? (
+                    <>
+                    <p>olá admin</p>
+                    </>
+                ) : (
+                    <>
+                        <section className={styles.info}>
+                            <img src={avatarUrl} alt="Avatar do usuário" className={styles.avatar} />
+                            <h2>SEJA BEM-VINDO, {user.name.split(' ')[0].toUpperCase()}!</h2>
+                            <div className={styles.content}>
+                                <label className={styles.labelfor}>Nome:</label>
+                                <p className={styles.txt}>{user.name.toUpperCase()}</p>
+                                <label className={styles.labelfor}>Email:</label>
+                                <span className={styles.txt}>{email}</span>
+                                <label className={styles.labelfor}>Função:</label>
+                                <span className={styles.txt}>{user.isstudent ? 'Estudante' : 'Funcionário'}</span>
+                                <button className={styles.button} onClick={
+                                    edit ? () => setEdit(false) : () => setEdit(true)
+                                }>{edit ? 'CANCELAR' : 'EDITAR'}</button>
+                            </div>
+                        </section>
+                    </>
+                )
+            }
         </article>
     );
 }
