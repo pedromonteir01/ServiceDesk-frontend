e121489272d9cb9a94307c28e22d16c47d775232
ArthBG <arthurborges.senai564@gmail.com>
2024-10-30 08:57:16 -0300

    Refactor styles in header and page modules

--- app/page.jsx
+++ app/page.jsx
@@ -24,6 +24,15 @@ export default function Home() {
           Video
         </h1>
       </div>
+      <div className={styles.containerBoth}>
+      <div className={styles.containerImage}>
+        <Image
+          src="/ilustracao.png"
+          alt="Pessoas se ajudando a consertar uma lampâda"
+          width={400}
+          height={300}
+          />
+          </div>
       <div className={styles.containerText}>
       <h1 className={styles.title}>
         Conheça a nossa funcionalidade 
@@ -31,13 +40,12 @@ export default function Home() {
       <p className={styles.description}>
        O Service Desk funciona como um "balcão de atendimento virtual", responsável por acolher e endereçar solicitações de suporte. Sua função é garantir a satisfação do cliente. Portanto, demanda agilidade, bom senso de priorização e capacidade de direcionamento e resolução efetiva. O que procuramos é a agilidade para processos de manutenção na infraestrutura do SENAI-Valinhos. 
       </p>
-      </div>
         <h1 className={styles.titleFunction}>
           Identificou alguma falha?
         </h1>
-      <div className={styles.buttonFunction}>
+      </div>
         <button className={styles.button}>Reportar Problema</button>
-        </div>
+      </div>
       </div>
       <Footer />
     </main>
