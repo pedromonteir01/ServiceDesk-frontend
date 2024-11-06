9d06b15622e6dcc1354434640b9fea859fd8bec8
ArthBG <arthurborges.senai564@gmail.com>
2024-11-06 15:22:05 -0300

    refactor: comment out axios configuration and related constants

--- app/config/axios.js
+++ app/config/axios.js
@@ -1,13 +1,14 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL;
-const USER_ID = 123;
+// const baseURL = 'https://localhost:4000/requests';
+// const USER_ID = 123;
 
-const axiosClient = axios.create({
-  baseURL,
-  headers: {
-    'x-user-id': USER_ID,
-  },
-});
+// const axiosClient = axios.create({
+//   baseURL,
+//   headers: {
+//     Authorization: `Bearer ${token}`,
+//     'x-user-id': USER_ID
+//   },
+// });
 
-export default axiosClient;
+// export default axiosClient;
