9d72b0c4dec3e7d6079139efc290fc12be9ef4a2
Gabriel Caurin <gabrielcaurin@gmail.com>
2024-11-06 16:49:27 -0300

    feat: fixing table responsive

--- app/Components/UserComponent/UserComponent.jsx
+++ app/Components/UserComponent/UserComponent.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "@/app/contexts/userContext";
@@ -8,36 +8,56 @@ import AdminPage from "../Admin/AdminPage";
 import { TailSpin } from "react-loader-spinner";
 
 const UserComponent = () => {
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setIsLoading(false);
-        }, 1000); 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
 
-        return () => clearTimeout(timer);
-    }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-    return (
+  return (
+    <>
+      {isLoading && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TailSpin
+            height="80"
+            width="80"
+            color="#ff0000"
+            ariaLabel="tail-spin-loading"
+          />
+        </div>
+      )}
+      {!isLoading && (
         <>
-            {isLoading && (
-                <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <TailSpin height="80" width="80" color="#ff0000" ariaLabel="tail-spin-loading" />
-                </div>
-            )}
-            {!isLoading && (
-                <>
-                    {user ? 
-                        user.isAdmin ? <AdminPage /> : <UserPage/>
-                    : 
-                        <LoginComponent/>
-                    }
-                </>
-            )}
+          {user ? (
+            user.isadmin ? (
+              <AdminPage />
+            ) : (
+              <UserPage />
+            )
+          ) : (
+            <LoginComponent />
+          )}
         </>
-    );
-}
+      )}
+    </>
+  );
+};
 
 export default UserComponent;
