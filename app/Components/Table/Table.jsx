3a3a6da17d0e1ecb578694447574d0abc12c6d85
Gabriel Caurin <gabrielcaurin@gmail.com>
2024-11-06 11:15:43 -0300

    feat: ajustar largura das tabelas e melhorar responsividade

--- app/Components/Table/Table.jsx
+++ app/Components/Table/Table.jsx
@@ -1,37 +1,35 @@
-import toast from 'react-hot-toast';
-import styles from './table.module.css';
+import toast from "react-hot-toast";
+import styles from "./table.module.css";
 
 const Table = ({ atributtes, content }) => {
-    if (!content || content.length === 0) {
-        return <p className={styles.none}>Nenhum resultado encontrado</p>;
-    }
+  if (!content || content.length === 0) {
+    return <p className={styles.none}>Nenhum resultado encontrado</p>;
+  }
 
-    return (
-        <table className={styles.container}>
-            <thead>
-                <tr className={styles.trow}>
-                    {
-                        atributtes.map((attribute, index) => (
-                            <th className={styles.theader} key={index}>{attribute.toUpperCase()}</th>
-                        ))
-                    }
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    content.map((obj, index) => (
-                        <tr key={index} className={styles.trow}>
-                            {
-                                Object.keys(obj).map((key, i) => (
-                                    <td className={styles.tdata} key={i}>{obj[key]}</td>
-                                ))
-                            }
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
-    );
-}
+  return (
+    <table className={styles.container}>
+      <thead>
+        <tr className={styles.trow}>
+          {atributtes.map((attribute, index) => (
+            <th className={styles.theader} key={index}>
+              {attribute.toUpperCase()}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className={styles.tbody}>
+        {content.map((obj, index) => (
+          <tr key={index} className={styles.trow}>
+            {Object.keys(obj).map((key, i) => (
+              <td className={styles.tdata} key={i}>
+                {obj[key]}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Table;
