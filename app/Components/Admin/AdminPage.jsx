f701f451a6caa88a9f214af1dd5461b6bcaeeb93
Caique <caiquenaimi.564@gmail.com>
2024-11-07 23:49:35 -0300

    feat: add framer-motion animations to modal in AdminPage and UserPage components

--- app/Components/Admin/AdminPage.jsx
+++ app/Components/Admin/AdminPage.jsx
@@ -336,10 +336,18 @@ const AdminPage = () => {
       </motion.section>
 
       {edit && (
-        <Modal closeModal={() => setEdit(false)} isOpen={edit}>
-          <ChangePassword />
-        </Modal>
-      )}
+                <Modal isOpen={edit} closeModal={() => setEdit(false)}>
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0.8 }}
+                        transition={{ duration: 0.5 }}
+                        className={styles.modalContent}
+                    >
+                        <ChangePassword />
+                    </motion.div>
+                </Modal>
+            )}
     </article>
   );
 };
--- app/Components/UserPage/UserPage.jsx
+++ app/Components/UserPage/UserPage.jsx
@@ -121,7 +121,15 @@ const UserPage = () => {
 
             {edit && (
                 <Modal isOpen={edit} closeModal={() => setEdit(false)}>
-                    <ChangePassword />
+                    <motion.div
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0.8 }}
+                        transition={{ duration: 0.5 }}
+                        className={styles.modalContent}
+                    >
+                        <ChangePassword />
+                    </motion.div>
                 </Modal>
             )}
         </article>
