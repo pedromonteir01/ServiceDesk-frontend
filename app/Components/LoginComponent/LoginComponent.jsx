261ef2c323ec6e73ada41c6df7939a0619e83fed
caiquenaimi <caique.naimi@aluno.senai.br>
2024-11-13 11:47:34 -0300

    feat: adicionar botão de cadastro no componente de login e ajustar estilos

--- app/Components/LoginComponent/LoginComponent.jsx
+++ app/Components/LoginComponent/LoginComponent.jsx
@@ -101,6 +101,16 @@ const LoginComponent = () => {
               </motion.button>
             </section>
           </motion.form>
+
+          <motion.button
+            className={style.btnNoAccount}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6, duration: 0.5 }}
+            onClick={() => router.replace("/Register")}
+          >
+            Não possui cadastro? Cadastre-se
+          </motion.button>
         </motion.article>
       </div>
     </div>
