02682b2499f28713d0bca1d2e50dd6a7e6b9a836
Gabriel Caurin de Souza <gabrielcaurin@gmail.com>
2024-11-13 08:32:19 -0300

    feat: arrumando coisa que o caique fez merda burro mds...

--- app/Components/SidebarItem/SidebarItem.jsx
+++ app/Components/SidebarItem/SidebarItem.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Link from 'next/link';
-import styles from './SidebarItem.module.css';
+import React from "react";
+import Link from "next/link";
+import styles from "./SidebarItem.module.css";
 
 const SidebarItem = ({ Icon, Text, href }) => {
   return (
     <Link className={styles.container} href={href}>
       <Icon />
-      <span style={{marginLeft: 15}}>{Text}</span>
+      <span>{Text}</span>
     </Link>
   );
 };
