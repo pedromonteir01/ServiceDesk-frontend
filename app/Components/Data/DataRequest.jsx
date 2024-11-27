9f406feba1cc5975c6919fd13f3bf4c57bbb9374
Gabriel Caurin <gabrielcaurin@gmail.com>
2024-11-27 15:12:15 -0300

    feat: estilização dos dados do mes anterior e mes seguinte

--- app/Components/Data/DataRequest.jsx
+++ app/Components/Data/DataRequest.jsx
@@ -1,23 +1,24 @@
-import styles from './dataRequest.module.css';
+import styles from "./dataRequest.module.css";
 
 const DataRequest = ({ month, requests, attented }) => {
-
-    const percent = () => {
-        if(requests == 0 || attented == 0) {
-            return false;
-        } else {
-            return `representando ${Math.floor((attented/requests) * 100)}% de resoluções este mês`;
-        }
+  const percent = () => {
+    if (requests == 0 || attented == 0) {
+      return false;
+    } else {
+      return `representando ${Math.floor(
+        (attented / requests) * 100
+      )}% de resoluções este mês`;
     }
+  };
 
-    return (
-        <div className={styles.container}>
-            <p>No mês de {month}</p>
-            <p>foram feitas um total de {requests}</p>
-            <p>onde {attented} foram atendidas</p>
-            <p>{percent()}</p>
-        </div>
-    );
-}
+  return (
+    <div className={styles.container}>
+      <p>
+        No mês de {month} foram feitas um total de {requests} onde {attented}{" "}
+        foram atendidas{percent()}
+      </p>
+    </div>
+  );
+};
 
 export default DataRequest;
