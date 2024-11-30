e7220a6f6956a3bfe86816ef4e6436c94fc51aca
ArthBG <arthurborges.senai564@gmail.com>
2024-11-30 19:55:04 -0300

    feat: replace favicon with custom SVG icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/senai.svg" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <UserProvider>
