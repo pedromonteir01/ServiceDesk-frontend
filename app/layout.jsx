41e5a6073588fc8a528523a388006400dc6c32ae
ArthBG <arthurborges.senai564@gmail.com>
2024-11-30 19:57:03 -0300

    feat: update favicon link to use favicon.ico

--- app/layout.jsx
+++ app/layout.jsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/senai.svg" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <UserProvider>
